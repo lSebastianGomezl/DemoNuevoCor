4ed29bf7192a600d83a82883100bdcbf13a84a00
notificaciones gitlab <notificaciones.gitlab@dcsas.com.co>
2024-06-07 14:58:57 -0500

    se inicializa la url

--- src/main/java/co/com/konex/cetificacion/login/backoffice/userinterfaces/login/PaginaBackoffice.java
+++ src/main/java/co/com/konex/cetificacion/login/backoffice/userinterfaces/login/PaginaBackoffice.java
@@ -1,9 +1,10 @@
 package co.com.konex.cetificacion.login.backoffice.userinterfaces.login;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+@DefaultUrl("https://dcsas-backoffice.konexinnovation.com/")
 public class PaginaBackoffice extends PageObject {
 
     public PaginaBackoffice() {
