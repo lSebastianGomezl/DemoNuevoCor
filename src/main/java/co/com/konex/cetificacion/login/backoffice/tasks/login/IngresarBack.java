9d0a85d03c7e2f6aa70bf36fe02d71e600c4879b
notificaciones gitlab <notificaciones.gitlab@dcsas.com.co>
2024-06-11 10:03:17 -0500

    update variables entorno

--- src/main/java/co/com/konex/cetificacion/login/backoffice/tasks/login/IngresarBack.java
+++ src/main/java/co/com/konex/cetificacion/login/backoffice/tasks/login/IngresarBack.java
@@ -29,9 +29,9 @@ public class IngresarBack implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        String documentType = System.getenv(tablaUsuarios.getTipoDocumento());
-        String user = System.getenv(tablaUsuarios.getUsuario());
-        String password = System.getenv(tablaUsuarios.getContrasenna());
+        String documentType = System.getenv("TIPO_DOCUMENTO");
+        String user = System.getenv("USUARIO");
+        String password = System.getenv("CONTRASENNA");
 
         actor.attemptsTo(
                 Click.on(ContenedorDeObjetosLogin.TIPO_DOCUMENTO),
