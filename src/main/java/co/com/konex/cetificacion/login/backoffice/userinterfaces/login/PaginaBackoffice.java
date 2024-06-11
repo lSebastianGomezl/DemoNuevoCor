1865cab8cdab928d57fe9d804816bbeae06d17de
notificaciones gitlab <notificaciones.gitlab@dcsas.com.co>
2024-06-11 10:23:16 -0500

    update variables entorno url

--- src/main/java/co/com/konex/cetificacion/login/backoffice/userinterfaces/login/PaginaBackoffice.java
+++ src/main/java/co/com/konex/cetificacion/login/backoffice/userinterfaces/login/PaginaBackoffice.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-@DefaultUrl("https://dcsas-backoffice.konexinnovation.com/")
+
 public class PaginaBackoffice extends PageObject {
 
     public PaginaBackoffice() {
